583e448c12b68741beae659dd0378f7e18801509
toxxicjames <ibezim.dev@gmail.com>
2024-09-17 22:45:00 +0200

    new changes to be modified.

---
+++ main.js
@@ -0,0 +1,29 @@
+document.getElementById('share-icon').addEventListener('click', function() {
+    const subFooter = document.getElementById('sub-footer');
+    const root = document.querySelector(":root");
+    const rootStyles = getComputedStyle(root);
+    const desaturatedDarkBlue = rootStyles.getPropertyValue("--DesaturatedDarkBlue");
+
+    subFooter.innerHTML = `
+        <div class="hovering-footer">
+            <h3 class="share-footer">SHARE</h3>
+            <a onclick="window.open('https://www.facebook.com', '_blank')" class="facebook-social-icon">
+                <img src="./images/icon-facebook.svg" alt="facebook" class="facebook">
+            </a>
+            <a onclick="window.open('https://www.x.com', '_blank')" class="twitter-social-icon">
+                <img src="./images/icon-twitter.svg" alt="twitter" class="twitter">
+            </a>
+            <a onclick="window.open('https://www.pinterest.com', '_blank')" class="pinterest-social-icon">
+                <img src="./images/icon-pinterest.svg" alt="pinterest" class="pinterest">
+            </a>
+            <button class="share-icon" id="share-icon">
+            <img 
+                src="./images/icon-share.svg" 
+                alt="share" 
+                class="share"
+            >
+        </button>
+        </div>
+    `;
+    document.querySelector('.hovering-footer').style.backgroundColor = desaturatedDarkBlue;
+});
