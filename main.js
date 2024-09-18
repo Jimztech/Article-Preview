0fbe6f5cedb40516ee778a566dc8093c2acb3d5a
toxxicjames <ibezim.dev@gmail.com>
2024-09-18 05:39:28 +0200

    updated

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
-document.getElementById('share-icon').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const subFooter = document.getElementById('applied');
     const root = document.querySelector(":root");
     const rootStyles = getComputedStyle(root);
     const desaturatedDarkBlue = rootStyles.getPropertyValue("--DesaturatedDarkBlue");
 
-    subFooter.innerHTML = `
-        <div class="hovering-footer">
-            <h3 class="share-footer">SHARE</h3>
-            <div class="hovering-icons">
-                <a onclick="window.open('https://www.facebook.com', '_blank')" class="facebook-social-icon">
-                    <img src="./images/icon-facebook.svg" alt="facebook" class="facebook">
-                </a>
-                <a onclick="window.open('https://www.x.com', '_blank')" class="twitter-social-icon">
-                    <img src="./images/icon-twitter.svg" alt="twitter" class="twitter">
-                </a>
-                <a onclick="window.open('https://www.pinterest.com', '_blank')" class="pinterest-social-icon">
-                    <img src="./images/icon-pinterest.svg" alt="pinterest" class="pinterest">
-                </a>
+    const originalSubFooter = subFooter.innerHTML;
+
+    function showHoveringFooter () {
+        subFooter.innerHTML = `
+            <div class="hovering-footer">
+                <h3 class="share-footer">SHARE</h3>
+                <div class="hovering-icons">
+                    <a onclick="window.open('https://www.facebook.com', '_blank')" class="facebook-social-icon">
+                        <img src="./images/icon-facebook.svg" alt="facebook" class="facebook">
+                    </a>
+                    <a onclick="window.open('https://www.x.com', '_blank')" class="twitter-social-icon">
+                        <img src="./images/icon-twitter.svg" alt="twitter" class="twitter">
+                    </a>
+                    <a onclick="window.open('https://www.pinterest.com', '_blank')" class="pinterest-social-icon">
+                        <img src="./images/icon-pinterest.svg" alt="pinterest" class="pinterest">
+                    </a>
+                </div>
+                <button class="share-icon" id="share-icon-hovering">
+                <img 
+                    src="./images/icon-share.svg" 
+                    alt="share" 
+                    class="share"
+                >
+                </button>
             </div>
-            <button class="share-icon" id="share-icon">
-            <img 
-                src="./images/icon-share.svg" 
-                alt="share" 
-                class="share"
-            >
-            </button>
-        </div>
-    `;
-    document.querySelector('.hovering-footer').style.backgroundColor = desaturatedDarkBlue;
+        `;
+        document.querySelector('.hovering-footer').style.backgroundColor = desaturatedDarkBlue;
+        document.getElementById('share-icon-hovering').addEventListener('click', showSubFooter);
+    }
+    
+
+    function showSubFooter() {
+        subFooter.innerHTML = originalSubFooter;
+        document.getElementById('share-icon').addEventListener('click', showHoveringFooter);
+    }
+    document.getElementById('share-icon').addEventListener('click', showHoveringFooter);
 });
-
-
-
